6c6f9340a363b443c662b19ca647517f9ef1cab0
rich.peters <rpeters@micro-magic.com>
2019-03-14 20:17:47 -0700

    tweak

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -3,9 +3,9 @@ const assert = require('assert');
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io');
-        const title = $('//title');
-        browser.waitUntil(() => {
-            return browser.getTitle() === 'WebdriverIO · Next-gen WebDriver test framework for Node.js'
-        }, 5000, 'expected title to be present');
+        const elem = $('//small');
+        elem.waitForDisplayed(3000);
+        const title = browser.getTitle();
+        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
 });
